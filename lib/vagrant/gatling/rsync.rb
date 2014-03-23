d46bc0eb7ed0640aff2d37de1338d86761ac37bd
Steven Merrill <steven.merrill@gmail.com>
2014-03-23 09:06:08 -0400

    Initial commit.

---
+++ lib/vagrant/gatling/rsync.rb
@@ -0,0 +1,9 @@
+require "vagrant/gatling/rsync/version"
+
+module Vagrant
+  module Gatling
+    module Rsync
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/vagrant/gatling/rsync/version.rb
@@ -0,0 +1,7 @@
+module Vagrant
+  module Gatling
+    module Rsync
+      VERSION = "0.0.1"
+    end
+  end
+end
