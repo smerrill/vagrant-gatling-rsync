475d9bb8c03588d3abc758e80e7c3819d675311d
Steven Merrill <steven.merrill@gmail.com>
2014-03-24 07:35:06 -0400

    Get the Windows adapter ready for testing.

--- lib/vagrant-gatling-rsync/listen/listenwindows.rb
+++ lib/vagrant-gatling-rsync/listen/listenwindows.rb
@@ -1,33 +1,46 @@
 # @TODO: Note that this is entirely untested and not yet implemented.
 
-require "timeout"
-require "thread"
+require "wdm"
 
-paths = {"/path/to/a/directory" => {}}
-latency = 2
+module VagrantPlugins
+  module GatlingRsync
+    class ListenWindows
+      def initialize(paths, ignores, latency, logger, callback)
+        @paths = paths
+        @ignores = ignores
+        @latency = latency
+        @logger = logger
+        @callback = callback
+      end
 
-monitor = WDM::Monitor.new
-changes = Queue.new
-paths.keys.each do |path|
-  monitor.watch_recursively(path) { |change| changes << change }
-end
-Thread.new { monitor.run! }
+      def run
+        @logger.info("Listening via: WDM on Windows.")
+        monitor = WDM::Monitor.new
+        changes = Queue.new
+        @paths.keys.each do |path|
+          monitor.watch_recursively(path) { |change| changes << change }
+        end
+        Thread.new { monitor.run! }
 
-loop do
-  directories = Set.new
-  begin
-    loop do
-      change = Timeout::timeout(latency) {
-        changes.pop
-      }
-      directories << change.path
+        loop do
+          directories = Set.new
+          begin
+            loop do
+              change = Timeout::timeout(@latency) {
+                changes.pop
+              }
+              directories << change.path
+            end
+          rescue Timeout::Error
+            @logger.info("Breaking out of the loop at #{Time.now.to_s}.")
+          end
+
+          @logger.info(directories.inspect) unless directories.empty?
+
+          @callback.call(@paths, @ignores, directories) unless directories.empty?
+        end
+      end
     end
-  rescue Timeout::Error
-    @logger.info("Breaking out of the loop at #{Time.now.to_s}.")
   end
-
-  @logger.info(directories.inspect) unless directories.empty?
-
-  #@callback.call(paths, ignores, directories) unless directories.empty?
 end
 
