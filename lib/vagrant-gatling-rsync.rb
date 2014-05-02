4925ac8322abb30d51bc0dc0c3d53cfd1089e906
Sam Boyer <tech@samboyer.org>
2014-05-02 14:32:55 -0400

    browbeating
    English, bro

--- lib/vagrant-gatling-rsync.rb
+++ lib/vagrant-gatling-rsync.rb
@@ -1,5 +1,5 @@
 # This file is required because Vagrant's plugin system expects
-# a eponymous ruby file matching the rubygem.
+# an eponymous ruby file matching the rubygem.
 #
 # So this gem is called 'vagrant-gatling-rsync' and thus vagrant tries
 # to require "vagrant-gatling-rsync"
